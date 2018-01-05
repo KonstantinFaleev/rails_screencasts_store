b9105931b8716254c6acfddd53a3b71f70510e8c
KonstantinFaleev <konstantin.faleev@gmail.com>
2018-01-06 00:12:50 +0200

    Callbacks model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,14 @@
 class Item < ApplicationRecord
   validates :price, numericality: { greater_than: 0, allow_nil: true }
   validates :name, :description, presence: true
+
+  belongs_to :category
+
+  after_initialize { puts "initialized" } # Item.new; Item.first
+  after_save       { puts "saved" } # Item.save; Item.create; item.update_attributes()
+  after_create     { ItemMailer.new_item_created(self).deliver }
+  after_create     { category.inc(:items_count, 1) }
+  after_update     { puts "updated" }
+  after_destroy    { category.inc(:items_count, -1) } # item.destroy
+
 end
