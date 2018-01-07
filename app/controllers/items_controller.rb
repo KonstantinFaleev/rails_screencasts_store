a0505a8e6622f052f79e82cd2e35828b8cb7fa66
KonstantinFaleev <konstantin.faleev@gmail.com>
2018-01-07 18:14:48 +0200

    RESTful controllers

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,8 +5,29 @@ class ItemsController < ApplicationController
     render text: @items.map { |i| "#{i.name}: #{i.price}" }.join("<br/>")
   end
 
+  # /items/1 GET
+  def show
+
+  end
+
+  # items/new GET
+  def new
+
+  end
+
+  # /item/1/edit GET
+  def edit
+
+  end
+
+  # /items POST
   def create
     @item = Item.create(params[:item])
     render text: "#{@item.id}: #{@item.name} (#{!@item.new_record?})"
+  end
+
+  #/items DELETE
+  def destroy
+
   end
 end
