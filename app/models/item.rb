b22c2cedba150517a48a6e22029555aefe8fb348
KonstantinFaleev <konstantin.faleev@gmail.com>
2018-01-10 20:43:41 +0200

    has_and_belongs_to_many

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,15 @@
 class Item < ApplicationRecord
   validates :price, numericality: { greater_than: 0, allow_nil: true }
   validates :name, :description, presence: true
+s
+  has_and_belongs_to_many :carts
+  #belongs_to :category
 
-  belongs_to :category
-
-  after_initialize { puts "initialized" } # Item.new; Item.first
-  after_save       { puts "saved" } # Item.save; Item.create; item.update_attributes()
-  after_create     { ItemMailer.new_item_created(self).deliver }
-  after_create     { category.inc(:items_count, 1) }
-  after_update     { puts "updated" }
-  after_destroy    { category.inc(:items_count, -1) } # item.destroy
+  #after_initialize { puts "initialized" } # Item.new; Item.first
+  #after_save       { puts "saved" } # Item.save; Item.create; item.update_attributes()
+  #after_create     { ItemMailer.new_item_created(self).deliver }
+  #after_create     { category.inc(:items_count, 1) }
+  #after_update     { puts "updated" }
+  #after_destroy    { category.inc(:items_count, -1) } # item.destroy
 
 end
